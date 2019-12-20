d2adb8f49a836d48ce7100a6495e4510e7b445a0
plasticrake <code@plasticrake.com>
2019-12-20 14:58:41 -0800

    style: add prettier, eslint, editorconfig and target es5

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    browser: false,
+    node: false,
+  },
+  extends: ['airbnb-base/legacy', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 5,
+  },
+};
